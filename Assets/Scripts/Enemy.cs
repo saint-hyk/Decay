8f929af26f4fbb88bdd43abcbc980d8024c7a33c
Harold Absalom <edward.absalom@gmail.com>
2015-04-18 12:21:42 +0200

    Initial Commit
    A bunch of crosses that move towards the centre of the screen.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------
+// Filename: Enemy.cs
+// Author:   Harold Absalom
+// Licence:  GNU General Public License
+// -----------------------------------------------
+
+using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+   #region properties
+
+   public Vector3 Destination {
+      get { return _destination; }
+      set { _destination = value; }
+   }
+
+   #endregion properties
+
+   #region fields
+
+   private Vector3 _destination;
+   private float _speed = 0.001f;
+
+   #endregion fields
+
+   #region methods
+
+   #region unity methods
+
+   private void Start() {
+   }
+
+   private void Update() {
+      Vector3 direction = (_destination - transform.position).normalized;
+      transform.Translate(direction * _speed);
+   }
+
+   #endregion unity methods
+
+   #endregion methods
+}
---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------
+// Filename: EnemyGenerator.cs
+// Author:   Harold Absalom
+// Licence:  GNU General Public License
+// -----------------------------------------------
+
+using System.Collections;
+using System.Timers;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+   #region properties
+
+   public Bounds SpawnBounds {
+      get { return _spawnBounds; }
+   }
+
+   #endregion properties
+
+   #region fields
+
+   [SerializeField]
+   private GameObject _enemyPrefab;
+
+   [SerializeField]
+   private Bounds _spawnBounds;
+
+   [SerializeField]
+   private float _minSpawnTime;
+
+   [SerializeField]
+   private float _maxSpawnTime;
+
+   private float _currentSpawnTime;
+   private float _elapsedSpawnTime = 0;
+
+   #endregion fields
+
+   #region methods
+
+   #region unity methods
+
+   private void Start() {
+      SetTimerInterval();
+   }
+
+   private void Update() {
+      _elapsedSpawnTime += Time.deltaTime;
+
+      if (_elapsedSpawnTime >= _currentSpawnTime) {
+         SpawnEnemy();
+         _elapsedSpawnTime = 0;
+         SetTimerInterval();
+      }
+   }
+
+   private void SpawnEnemy() {
+      bool major = Random.value > 0.5f;
+      bool minor = Random.value > 0.5f;
+
+      float xPos;
+      float yPos;
+
+      if (major) {
+         xPos = Random.Range(_spawnBounds.min.x, _spawnBounds.max.x);
+         yPos = (minor ? _spawnBounds.min.y : _spawnBounds.max.y);
+      } else {
+         xPos = (minor ? _spawnBounds.min.x : _spawnBounds.max.x);
+         yPos = Random.Range(_spawnBounds.min.y, _spawnBounds.max.y);
+      }
+
+      Vector3 spawnPos = new Vector3(xPos, yPos, 0);
+      GameObject enemy = GameObject.Instantiate(_enemyPrefab, spawnPos, Quaternion.identity) as GameObject;
+      enemy.GetComponent<Enemy>().Destination = _spawnBounds.center;
+   }
+
+   private void SetTimerInterval() {
+      _currentSpawnTime = Random.Range(_minSpawnTime, _maxSpawnTime);
+   }
+
+   #endregion unity methods
+
+   #endregion methods
+}
